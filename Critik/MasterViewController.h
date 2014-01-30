462931aa7ff326fb160e68372955f6cb10d94e38
Ryan Taylor <ryan.taylor976@gmail.com>
2014-01-30 00:13:44 -0600

    Committing repository

---
+++ Critik/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Critik
+//
+//  Created by Dalton Decker on 1/24/14.
+//  Copyright (c) 2014 RedVelvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
