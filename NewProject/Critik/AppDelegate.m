a8e6cd7b8664aefe9de3c28a505f3e6baea5ebe4
Dalton <s507862@mail.nwmissouri.edu>
2014-02-11 17:49:10 -0600

    Began Creating Layout for Storyboard. Created. Home, Edit Forms, Evaluate Students, Choose Speech, Section. etc.

--- NewProject/Critik/AppDelegate.m
+++ NewProject/Critik/AppDelegate.m
@@ -18,17 +18,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    splitViewController.delegate = (id)navigationController.topViewController;
-
-    UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
-    MasterViewController *controller = (MasterViewController *)masterNavigationController.topViewController;
-    controller.managedObjectContext = self.managedObjectContext;
+//    // Override point for customization after application launch.
+//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+//    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+//    splitViewController.delegate = (id)navigationController.topViewController;
+//
+//    UINavigationController *masterNavigationController = splitViewController.viewControllers[0];
+//    MasterViewController *controller = (MasterViewController *)masterNavigationController.topViewController;
+//    controller.managedObjectContext = self.managedObjectContext;
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
