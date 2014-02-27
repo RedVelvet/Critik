9e45bf079f8df9129a23f464b500e0cefd81b002
dwettlaufer <dwettlaufer@me.com>
2014-02-27 17:28:52 -0600

    First commit for EditPersuasive

---
+++ NewProject/Critik/EditPersuasiveVC.h
@@ -0,0 +1,13 @@
+//
+//  EditPersuasiveVC.h
+//  Critik
+//
+//  Created by Doug Wettlaufer on 2/27/14.
+//  Copyright (c) 2014 RedVelvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditPersuasiveVC : UIViewController
+
+@end
---
+++ NewProject/Critik/EditPersuasiveVC.m
@@ -0,0 +1,38 @@
+//
+//  EditPersuasiveVC.m
+//  Critik
+//
+//  Created by Doug Wettlaufer on 2/27/14.
+//  Copyright (c) 2014 RedVelvet. All rights reserved.
+//
+
+#import "EditPersuasiveVC.h"
+
+@interface EditPersuasiveVC ()
+
+@end
+
+@implementation EditPersuasiveVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
