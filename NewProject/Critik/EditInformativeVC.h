d6adc09058656df997f4bfbe9c17f7754a1757b1
Dalton <s507862@mail.nwmissouri.edu>
2014-02-27 17:25:34 -0600

    Initial Commit. Created EditInformativeVC

---
+++ NewProject/Critik/EditInformativeVC.h
@@ -0,0 +1,13 @@
+//
+//  EditInformativeVC.h
+//  Critik
+//
+//  Created by Dalton Decker on 2/27/14.
+//  Copyright (c) 2014 RedVelvet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditInformativeVC : UIViewController
+
+@end
