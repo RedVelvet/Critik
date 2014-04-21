8122314d7073b3b60cf83dc11ab89336ca512566
Dalton <ddecker2010@gmail.com>
2014-04-20 21:50:26 -0500

    Changed Category colors to match

--- NewProject/Critik/StudentPenaltiesVC.m
+++ NewProject/Critik/StudentPenaltiesVC.m
@@ -59,131 +59,75 @@
 - (IBAction)Finalize:(id)sender
 {
     
-    //Create new Finalize view controller and push to view with currentStudent and currentStudentSpeech
-    SpeechFinalizeVC * finalize = [self.storyboard instantiateViewControllerWithIdentifier:@"Finalize"];
-    finalize.currentStudent = self.currentStudent;
-    finalize.currentStudentSpeech = self.currentStudentSpeech;
-    [self.navigationController pushViewController:finalize animated:YES];
+    //Create error variable to use for saving to core data
+    NSError * error;
     
-    
+    if([self.penaltyPoints.text intValue] < 0){
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Error" message: @"Penalties can not be negative" delegate: nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+        
+    }else{
+        
+        //Save duration, comments and penalty points to core data
+        self.currentStudentSpeech.duration = [NSNumber numberWithInt:[self.penaltyPoints.text intValue]];
+        self.currentStudentSpeech.comments = self.additionalComments.text;
+        self.currentStudentSpeech.penaltyPoints = [NSNumber numberWithInt: [self.penaltyPoints.text intValue]];
+        
+        self.currentStudentSpeech.penaltyPoints = [NSNumber numberWithInt:[self.penaltyPoints.text intValue]];
+        
+        //Generate points earned, and total points based on penalty points
+        NSArray * allModules = [self.currentStudentSpeech.speech.modules allObjects];
+        int pointsEarned = 0;
+        int pointsPossible = 0;
+        for(int i = 0; i < [allModules count]; i++){
+            Module * currentModule = [allModules objectAtIndex:i];
+            
+            pointsEarned += [currentModule.points intValue];
+            pointsPossible += [currentModule.pointsPossible intValue];
+        }
+        
+        self.currentStudentSpeech.pointsEarned = [NSNumber numberWithInt:pointsEarned];
+        self.currentStudentSpeech.totalPoints = [NSNumber numberWithInt:(pointsEarned-[self.penaltyPoints.text intValue])];
+        
+        //if switch is selected for late presentation, store true in core data for speech being late
+        if(self.latePresentation.isOn){
+            self.currentStudentSpeech.isLate = @"true";
+        }else{
+            self.currentStudentSpeech.isLate = @"false";
+        }
+        
+        //if switch is selected for over time, store true in core data for speech not meeting time constraints
+        if(self.overTime.isOn){
+            self.currentStudentSpeech.overTime = @"true";
+        }else{
+            self.currentStudentSpeech.overTime = @"false";
+        }
+        
+        //Takes addtional comments professor types in and stores in core data with speech
+        self.currentStudentSpeech.comments = self.additionalComments.text;
+        
+        //save managagedObjectContext
+        if(![self.managedObjectContext save:&error])
+        {
+            NSLog(@"Can't Save duration %@",[error localizedDescription]);
+        }
+        
+        
+        //Create new Finalize view controller and push to view with currentStudent and currentStudentSpeech
+        SpeechFinalizeVC * finalize = [self.storyboard instantiateViewControllerWithIdentifier:@"Finalize"];
+        finalize.currentStudent = self.currentStudent;
+        finalize.currentStudentSpeech = self.currentStudentSpeech;
+        [self.navigationController pushViewController:finalize animated:YES];
+    }
+
 }
 
 #pragma mark Keyboard
-//-(void)keyboardWillShow {
-//    // Animate the current view out of the way
-//    if (self.view.frame.origin.y >= 0)
-//    {
-//        [self setViewMovedUp:YES];
-//    }
-//    else if (self.view.frame.origin.y < 0)
-//    {
-//        [self setViewMovedUp:NO];
-//    }
-//}
-//
-//-(void)keyboardWillHide {
-//    if (self.view.frame.origin.y >= 0)
-//    {
-//        [self setViewMovedUp:YES];
-//    }
-//    else if (self.view.frame.origin.y < 0)
-//    {
-//        [self setViewMovedUp:NO];
-//    }
-//}
-//
-//-(void)textFieldDidBeginEditing:(UITextField *)sender
-//{
-//    if ([sender tag] == 1)
-//    {
-//        //move the main view, so that the keyboard does not hide it.
-//        if  (self.view.frame.origin.y >= 0 )
-//        {
-//            [self setViewMovedUp:YES];
-//        }
-//    }
-//}
-//
-////method to move the view up/down whenever the keyboard is shown/dismissed
-//-(void)setViewMovedUp:(BOOL)movedUp
-//{
-//    [UIView beginAnimations:nil context:NULL];
-//    [UIView setAnimationDuration:0.3]; // if you want to slide up the view
-//    
-//    CGRect rect = self.view.frame;
-//    if (movedUp)
-//    {
-//        // 1. move the view's origin up so that the text field that will be hidden come above the keyboard
-//        // 2. increase the size of the view so that the area behind the keyboard is covered up.
-//        rect.origin.y -= kOFFSET_FOR_KEYBOARD;
-//        rect.size.height += kOFFSET_FOR_KEYBOARD;
-//    }
-//    else
-//    {
-//        // revert back to the normal state.
-//        rect.origin.y += kOFFSET_FOR_KEYBOARD;
-//        rect.size.height -= kOFFSET_FOR_KEYBOARD;
-//    }
-//    self.view.frame = rect;
-//    
-//    [UIView commitAnimations];
-//}
-
-
-//- (void)viewWillAppear:(BOOL)animated
-//{
-//    // register for keyboard notifications
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow) name:UIKeyboardWillShowNotification object:nil];
-//    
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide) name:UIKeyboardWillHideNotification object:nil];
-//}
 
 - (void)viewWillDisappear:(BOOL)animated
 {
-    //Create error variable to use for saving to core data
-    NSError * error;
-    NSLog(@"Duration:%@",self.currentStudentSpeech.duration);
-    //Save duration, comments and penalty points to core data
-    self.currentStudentSpeech.duration = [NSNumber numberWithInt:[self.penaltyPoints.text intValue]];
-    self.currentStudentSpeech.comments = self.additionalComments.text;
-    self.currentStudentSpeech.penaltyPoints = [NSNumber numberWithInt:[self.penaltyPoints.text intValue]];
-    
-    //Generate points earned, and total points based on penalty points
-    NSArray * allModules = [self.currentStudentSpeech.speech.modules allObjects];
-    int pointsEarned = 0;
-    int pointsPossible = 0;
-    for(int i = 0; i < [allModules count]; i++){
-        Module * currentModule = [allModules objectAtIndex:i];
-        
-        pointsEarned += [currentModule.points intValue];
-        pointsPossible += [currentModule.pointsPossible intValue];
-    }
-    
-    self.currentStudentSpeech.pointsEarned = [NSNumber numberWithInt:pointsEarned];
-    self.currentStudentSpeech.totalPoints = [NSNumber numberWithInt:(pointsEarned-[self.penaltyPoints.text intValue])];
-    
-    //if switch is selected for late presentation, store true in core data for speech being late
-    if(self.latePresentation.isOn){
-        self.currentStudentSpeech.isLate = @"true";
-    }else{
-        self.currentStudentSpeech.isLate = @"false";
-    }
-    
-    //if switch is selected for over time, store true in core data for speech not meeting time constraints
-    if(self.overTime.isOn){
-        self.currentStudentSpeech.overTime = @"true";
-    }else{
-        self.currentStudentSpeech.overTime = @"false";
-    }
-    
-    //Takes addtional comments professor types in and stores in core data with speech
-    self.currentStudentSpeech.comments = self.additionalComments.text;
-    
-    //save managagedObjectContext
-    if(![self.managedObjectContext save:&error])
-    {
-        NSLog(@"Can't Save duration %@",[error localizedDescription]);
-    }
+
     // unregister for keyboard notifications while not visible.
 //    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
 //    
